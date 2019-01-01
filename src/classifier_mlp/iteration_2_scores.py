cfdd005525e0e5870a721dd69f2a6b96be372bf1
Riley Littlefield <rileylittlefield@ymail.com>
2019-01-01 03:14:29 -0500

    adds iteration_2 test set scores for mlp

--- src/classifier_mlp/iteration_2_scores.py
+++ src/classifier_mlp/iteration_2_scores.py
@@ -1,22 +1,41 @@
 from classifier_mlp.iteration_2_load_clf import mlp_classifier
-from data_management.train_set import number_classes, flat_images
+from data_management.train_set import number_classes as number_classes_train, flat_images as flat_images_train
+from data_management.test_set import number_classes as number_classes_test, flat_images as flat_images_test
 from sklearn.metrics import precision_score, recall_score
 
-predictions = mlp_classifier.predict(flat_images)
+predictions_train = mlp_classifier.predict(flat_images_train)
+predictions_test = mlp_classifier.predict(flat_images_test)
 
-average_precision = precision_score (
-    number_classes,
-    predictions,
+average_precision_train = precision_score (
+    number_classes_train,
+    predictions_train,
     average='macro'
 )
 
-average_recall = recall_score(
-    number_classes,
-    predictions,
+average_precision_test = precision_score (
+    number_classes_test,
+    predictions_test,
     average='macro'
 )
 
-print('average_precision = ', average_precision)
-# average_precision =  0.9597057833216434
-print('average_recall = ', average_recall)
-# average_recall =  0.9595116892911011
+average_recall_train = recall_score(
+    number_classes_train,
+    predictions_train,
+    average='macro'
+)
+
+average_recall_test = recall_score(
+    number_classes_test,
+    predictions_test,
+    average='macro'
+)
+
+print('average_precision_train = ', average_precision_train)
+# average_precision_train =  0.9597057833216434
+print('average_recall_train = ', average_recall_train)
+# average_recall_train =  0.9595116892911011
+
+print('average_precision_train = ', average_precision_test)
+# average_precision_train =  0.9032268031085339
+print('average_recall_test = ', average_recall_test)
+# average_recall_test =  0.9024839743589742
